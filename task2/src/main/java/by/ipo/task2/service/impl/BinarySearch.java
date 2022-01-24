edb8e22efebd6b5fe15c25f8ced09c19a15836ff
D2tun <71644776+D2tun@users.noreply.github.com>
2022-01-24 10:12:37 +0300

    Update BinarySearch.java

--- task2/src/main/java/by/ipo/task2/service/impl/BinarySearch.java
+++ task2/src/main/java/by/ipo/task2/service/impl/BinarySearch.java
@@ -9,9 +9,9 @@ public class BinarySearch {
 		int leftBorder = 0;
 		while (leftBorder <= rightBorder) {
 			int middle = leftBorder + ((rightBorder - leftBorder) / 2);
-			if (array.getParameter(array.getIndex(number)) > number) {
+			if (array.getElement(array.getIndex(number)) > number) {
 				rightBorder = middle - 1;
-			} else if (array.getParameter(array.getIndex(number)) < number) {
+			} else if (array.getElement(array.getIndex(number)) < number) {
 				leftBorder = middle + 1;
 			} else {
 				return middle;
