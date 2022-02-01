a8cac85aabd7c4526bca30298e7991fefda6f53c
D2tun <71644776+D2tun@users.noreply.github.com>
2022-02-01 10:36:47 +0300

    Add files via upload

--- task2/src/main/java/by/ipo/task2/service/impl/ShellSort.java
+++ task2/src/main/java/by/ipo/task2/service/impl/ShellSort.java
@@ -1,6 +1,6 @@
-package by.ipo.task2.service.impl;
+package by.ipo.task5.service.impl;
 
-import by.ipo.task2.bean.Array;
+import by.ipo.task5.bean.Array;
 
 /**
  * This class provides method for Shell's sorting.
@@ -8,28 +8,34 @@ import by.ipo.task2.bean.Array;
  * @see Array
  */
 public class ShellSort {
-	
+
 	/**
-	 * This method sorts array with shaker sort.
+	 * This method sorts array with Shell's sort.
 	 * @param array - unsorted array
 	 * @return sorted array
 	 */
-	public static  <T> Array<T> sort(Array<T> array) {
-		
-		 for (int gap = array.getLength() / 2; gap > 0; gap /= 2) {
-		        for (int i = gap; i < array.getLength(); i++) {
-		        	T key = array.getElement(i);
-		            int j = i;
-		            while ((j >= gap) && (array.getElement(j - gap).toString()
-		            						.compareTo(key.toString()) 
-		            		< 0)) {
-		            	array.swapElements(j, j - gap);
-		                j -= gap;
-		            }
-		            array.setElement(j, key);
-		        }
-		    }
-		
+	public static Array<Double> sort(Array<Double> array) {
+
+		int h = 1;
+		while (h < array.getLength()) {
+			h = 3 * h + 1;
+		}
+
+		while (h > 0) {
+			h = h / 3;
+			for (int k = 0; k < h; k++) {
+				for (int i = h + k; i < array.getLength(); i += h) {
+					double key = array.getElement(i);
+					int j = i - h;
+					while (j >= 0 && array.getElement(j) > key) {
+						array.setElement(j + h, array.getElement(j));
+						j -= h;
+					}
+					array.setElement(j + h, key);
+				}
+			}
+		}
+
 		return array;
 	}
 }
